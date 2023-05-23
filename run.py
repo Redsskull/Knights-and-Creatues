4890f3942f0e39e590efaf845a84b6457a31beab
Redsskull <ivry91@gmail.com>
2023-05-23 02:42:56 +0200

    refactor all functions into a proper main function
    some of the cose was not in a function, and some variables were global. all code to start the game with text is now in one function that is called from the main

--- run.py
+++ run.py
@@ -53,74 +53,75 @@ class WaterElemental():
         return self.name
         
 
-#creating the player instance on start game
-
-print("In search for Bartholomew also known as 'Bart the claw cuddler'")
-print("Who will start this epic journey to find Bart and help him?\n")
-player_name = input("Enter your name: ")
-print("Choose your class")
-print('''1. Warrior
-------
-When I was young I enjoyed playing with sticks and stones, fighting and defeating all of my father's scarecrows, 
-dreamed about becoming a powerful warrior that defeats his foes and now here I am, ready for my next challenge!\n''')
-print('''2.Mage 
-------
-Are you thirsty? sorry, my water is not drinkable!
-Hungry? I can... cook... our enemies!
-Travel from town to town in a blink of an eye? show me your money!\n''')
-
-print('''3.Bard 
-------
-In a realm of Myst
-Where the dreams come true,
-There we are in search of Bartholomew
-Also known as 'Bart the claw cuddler'
-
-And I have no idea how we will rescue him
-Deep in darkness is where we go
-Fingers crossed is what we hope
-Bart, here we come!\n''')
-
-print('''4.Clerk 
------
-In this dark world, instead of using swords, shields, or knives, I prefer to use my hands... to pray and heal others, 
-but also to unleash the holy power upon our enemies, I prefer to work with a group of misfits, but in the end, 
-if you pay me enough... I mean if you show me your great gratitude, I can go anywhere\n''')
-
-archetype_choice = int(input("Pick a number to choose your class: "))
-
-if archetype_choice == 1:
-    player_class = Warrior()
-elif archetype_choice == 2:
-    player_class = Mage()
-elif archetype_choice == 3:
-    player_class = Bard()
-else:
-    player_class = Clerk()
-
-
-player = Player(player_name,player_class)
-print("Player:", player_name)
-print("Archetype:", player_class)
-
-print('''Red needs your help to find his magical cat, Bartholomew, he was kidnaped by an evil enemy called Skull, he cannot do this alone, he needs PLAYER_NAME_HERE 
-to rescue Bart and defeat his enemy, to do that, you need to find the 3 gemstones and combine them to open a portal to the Void Prison where Bart is kept.''')
-
-#start game function
-
-
 def start_game():
+    print("In search for Bartholomew also known as 'Bart the claw cuddler'")
+    print("Who will start this epic journey to find Bart and help him?\n")
+    player_name = input("Enter your name: ")
+    print("Choose your class")
+    print('''1. Warrior
+    ------
+    When I was young I enjoyed playing with sticks and stones, fighting and defeating all of my father's scarecrows, 
+    dreamed about becoming a powerful warrior that defeats his foes and now here I am, ready for my next challenge!\n''')
+    print('''2.Mage 
+    ------
+    Are you thirsty? sorry, my water is not drinkable!
+    Hungry? I can... cook... our enemies!
+    Travel from town to town in a blink of an eye? show me your money!\n''')
+
+    print('''3.Bard 
+    ------
+    In a realm of Myst
+    Where the dreams come true,
+    There we are in search of Bartholomew
+    Also known as 'Bart the claw cuddler'
+
+    And I have no idea how we will rescue him
+    Deep in darkness is where we go
+    Fingers crossed is what we hope
+    Bart, here we come!\n''')
+
+    print('''4.Clerk 
+    -----
+    In this dark world, instead of using swords, shields, or knives, I prefer to use my hands... to pray and heal others, 
+    but also to unleash the holy power upon our enemies, I prefer to work with a group of misfits, but in the end, 
+    if you pay me enough... I mean if you show me your great gratitude, I can go anywhere\n''')
+
+
+    archetype_choice = int(input("Pick a number to choose your class: "))
+    player_class = None
+
+    if archetype_choice == 1:
+        player_class = Warrior()
+    elif archetype_choice == 2:
+        player_class = Mage()
+    elif archetype_choice == 3:
+        player_class = Bard()
+    elif archetype_choice == 4:
+        player_class = Clerk()
+    else:
+        print("Please select a number between 1 - 4 to choose your class")
+
+
+    player = Player(player_name,player_class)
+    print("Player:", player_name)
+    print("Archetype:", player_class)
+
+    print('''Red needs your help to find his magical cat, Bartholomew, he was kidnaped by an evil enemy called Skull, he cannot do this alone, he needs your
+    to rescue Bart and defeat his enemy, to do that, you need to find the 3 gemstones and combine them to open a portal to the Void Prison where Bart is kept.''')
+
     choice = ""
     while choice not in ["y", "n"]:
         choice = input("Are you up for this challenge? (y/n): ").lower()
         if choice == "y":
              return True
         elif choice == "n":
-             return False
+             print("Bart is upset!")
+             exit()
         else:
             print("Invalid input. Please enter 'y' for Yes or 'n' for No.")
 
 #first dungeon
+
 
 def blue_stone():
     print('''The blue stone is hidden inside an underwater cave, in an Amazonian forest, you start swimming inside the lake, and soon you encounter a few dolphins.''')
@@ -130,12 +131,13 @@ def blue_stone():
         choice = input("What will you do?\n A. Pretend you are just here for a nice relaxing dive and move forward\n B. Greet them and tell them what you searching for\n C. Ask them to help you in your journey to acquire the Blue Stone\n what is your choice?" ).upper()
         if choice == "A":
             print("You passed them, you found the cave and you enter it")
+            return True
         elif choice == "B":
             print("They became hostile when they found out you want the stone. You die.")
-            game_over = True
+            return False
         elif choice == "C":
             print("They helped you find the stone, and then they kill you to take it for themselves. You die")
-            game_over = True
+            return False
         else:
             print("Invalid input. Please select one of 'A', 'B' or 'C'")
     return not game_over
@@ -146,21 +148,24 @@ def blue_stone():
 
     
 def main():
-  
-  while True:
-  
-    if start_game():    
-        print("Let the adventure begin!")
-        if blue_stone():
-            print("Congratulations! You completed the game.")
-        else:
-            print("Game Over! Maybe next time!")
+    game_state = "start"
+    while game_state != "end":
+        if game_state == "start":
+            if start_game():
+                game_state = "blue_stone"
+            else:
+                game_state = "end"
+        elif game_state == "blue_stone":
+           if blue_stone():
+               game_state  == "blue_stone_two"
+           else:
+               game_state = "end"
     else:
-        print("Maybe next time!")
-
-    restart_choice = input("Do you want to play again? (y/n): ").lower()
-    if restart_choice != "y":
+        game_state = "end"
+    restart_choice = input("Do you want to restart the game? (y/n): ").lower()
+    if restart_choice == "y":
+        main()
+    else:
         print("Thanks for playing! Goodbye.")
-        break  
 
 main()
