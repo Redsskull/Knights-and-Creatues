a4c59e09fc3a946217d3b0ed3d30af95733d5b0b
Redsskull <ivry91@gmail.com>
2023-05-25 16:17:46 +0200

    add small easter egg to the game when the name Bart is picked

--- run.py
+++ run.py
@@ -3,8 +3,7 @@ import termcolor
 # I'm going to start by defining the assets(object) the game will use.\
 
 """
-TODO - deploy
-fix exception
+TODO  - fix exception
 Dungeon 4 and contend with water elemental class
 end the game
 Docstrings and README
@@ -81,65 +80,69 @@ def start_game():
     print("In search for Bartholomew also known as 'Bart the claw cuddler'")
     print("Who will start this epic journey to find Bart and help him?\n")
     player_name = input("Enter your name:\n ")
-    print("Choose your class")
-    print('''1. Warrior
-    ------
-    When I was young I enjoyed playing with sticks and stones, fighting and defeating all of my father's scarecrows, 
-    dreamed about becoming a powerful warrior that defeats his foes and now here I am, ready for my next challenge!\n''')
-    print('''2.Mage 
-    ------
-    Are you thirsty? sorry, my water is not drinkable!
-    Hungry? I can... cook... our enemies!
-    Travel from town to town in a blink of an eye? show me your money!\n''')
-
-    print('''3.Bard 
-    ------
-    In a realm of Myst
-    Where the dreams come true,
-    There we are in search of Bartholomew
-    Also known as 'Bart the claw cuddler'
-
-    And I have no idea how we will rescue him
-    Deep in darkness is where we go
-    Fingers crossed is what we hope
-    Bart, here we come!\n''')
-
-    print('''4.Clerk 
-    -----
-    In this dark world, instead of using swords, shields, or knives, I prefer to use my hands... to pray and heal others, 
-    but also to unleash the holy power upon our enemies, I prefer to work with a group of misfits, but in the end, 
-    if you pay me enough... I mean if you show me your great gratitude, I can go anywhere\n''')
-
-    archetype_choice = int(input("Pick a number to choose your class:\n "))
-    archetype = None
-
-    if archetype_choice == 1:
-        archetype = Warrior()
-    elif archetype_choice == 2:
-        archetype = Mage()
-    elif archetype_choice == 3:
-        archetype = Bard()
-    elif archetype_choice == 4:
-        archetype = Clerk()
+    if player_name == "bart".lower():
+        print("Oh, looks like the writer was mistaken, no rescue needed. Goodbye!")
+        exit()
     else:
-        print("Please select a number between 1 - 4 to choose your class")
+        print("Choose your class")
+        print('''1. Warrior
+        ------
+        When I was young I enjoyed playing with sticks and stones, fighting and defeating all of my father's scarecrows, 
+        dreamed about becoming a powerful warrior that defeats his foes and now here I am, ready for my next challenge!\n''')
+        print('''2.Mage 
+        ------
+        Are you thirsty? sorry, my water is not drinkable!
+        Hungry? I can... cook... our enemies!
+        Travel from town to town in a blink of an eye? show me your money!\n''')
 
-    player = Player(player_name, archetype.name)
-    player.archetype = archetype
+        print('''3.Bard 
+        ------
+        In a realm of Myst
+        Where the dreams come true,
+        There we are in search of Bartholomew
+        Also known as 'Bart the claw cuddler'
 
-    print('''Red needs your help to find his magical cat, Bartholomew, he was kidnaped by an evil enemy called Skull, he cannot do this alone, he needs your
-    to rescue Bart and defeat his enemy, to do that, you need to find the 3 gemstones and combine them to open a portal to the Void Prison where Bart is kept.''')
+        And I have no idea how we will rescue him
+        Deep in darkness is where we go
+        Fingers crossed is what we hope
+        Bart, here we come!\n''')
 
-    choice = ""
-    while choice not in ["y", "n"]:
-        choice = input("Are you up for this challenge? (y/n):\n ").lower()
-        if choice == "y":
-            return player
-        elif choice == "n":
-            print("Bart is upset!")
-            exit()
+        print('''4.Clerk 
+        -----
+        In this dark world, instead of using swords, shields, or knives, I prefer to use my hands... to pray and heal others, 
+        but also to unleash the holy power upon our enemies, I prefer to work with a group of misfits, but in the end, 
+        if you pay me enough... I mean if you show me your great gratitude, I can go anywhere\n''')
+
+        archetype_choice = int(input("Pick a number to choose your class:\n "))
+        archetype = None
+
+        if archetype_choice == 1:
+            archetype = Warrior()
+        elif archetype_choice == 2:
+            archetype = Mage()
+        elif archetype_choice == 3:
+            archetype = Bard()
+        elif archetype_choice == 4:
+            archetype = Clerk()
         else:
-            print("Invalid input. Please enter 'y' for Yes or 'n' for No.")
+            print("Please select a number between 1 - 4 to choose your class")
+
+        player = Player(player_name, archetype.name)
+        player.archetype = archetype
+
+        print('''Red needs your help to find his magical cat, Bartholomew, he was kidnaped by an evil enemy called Skull, he cannot do this alone, he needs your
+        to rescue Bart and defeat his enemy, to do that, you need to find the 3 gemstones and combine them to open a portal to the Void Prison where Bart is kept.''')
+
+        choice = ""
+        while choice not in ["y", "n"]:
+            choice = input("Are you up for this challenge? (y/n):\n ").lower()
+            if choice == "y":
+                return player
+            elif choice == "n":
+                print("Bart is upset!")
+                exit()
+            else:
+                print("Invalid input. Please enter 'y' for Yes or 'n' for No.")
 
 
 def player_choice(prompt, choices, outcomes, player):
